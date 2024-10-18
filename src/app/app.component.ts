8de3ac103933bdfeade09bc9f940860ef03ba414
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-10-18 16:37:19 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mentoring-program-starter-kit';
+  private httpClient: HttpClient = inject(HttpClient);
 }
+// https://fakestoreapi.com/
