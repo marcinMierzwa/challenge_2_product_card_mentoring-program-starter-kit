00f34c873fd50cd7c86dd04827bf940ff986ee5e
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-10-18 19:39:17 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,48 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { map, Observable, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
+export interface Product {
+  readonly category: string;
+  readonly image: string;
+  readonly id: number;
+  readonly price: number;
+  readonly title: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'mentoring-program-starter-kit';
   private httpClient: HttpClient = inject(HttpClient);
+  categories$: Observable<string[]> = this.httpClient.get<string[]>('https://fakestoreapi.com/products/categories');
+  products$: Observable<Product[]> = this.httpClient.get<any[]>('https://fakestoreapi.com/products')
+  .pipe(
+    map((products) => 
+      products.map((product) => ({
+        category: product.category,
+        image: product.image,
+        id: product.id,
+        price: product.price,
+        title: product.title
+      }))
+    )
+  );
+
 }
-// https://fakestoreapi.com/
+
+// fetch('https://fakestoreapi.com/products')
+// .then(res => res.json())
+// .then(data => console.log(data)
+// )
+
+// https://fakestoreapi.com/products/categories
+
+// https://fakestoreapi.com/products
